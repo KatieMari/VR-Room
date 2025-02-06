33d3bc94e192b5f743fc39463172f01cdbd57f01
KatieMari <katie.glover@student.norwichuni.ac.uk>
2025-02-06 19:25:24 +0000

    crying

--- Assets/Scripts/CustomBubbles.cs
+++ Assets/Scripts/CustomBubbles.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class CustomBubbles : MonoBehaviour
 
 {
-    [Tooltip("The object that will be spawned")]
-    public GameObject prefab = null;
-    [Tooltip("The transform where the object is spawned")]
-    public Transform spawnTransform = null;
-    public Void Spawn(){
-        // Creates Bubble
-       GameObject bubble =  Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
-    //    Rotates Randomly
-       bubble.transform.rotation = Random.rotation;
-    //    Set Random Launch Speed
-       float launchSpeed = Random.Range(0.5f, 1.5f);
-       Vector3 force = spawnTransform.forward * launchSpeed
-    }
+   [Tooltip("The object that will be spawned")]
+   public GameObject prefab = null;
+   [Tooltip("The transform where the object is spawned")]
+   public Transform spawnTransform = null;
+   public void Spawn()
+   {
+      // Creates Bubble
+      GameObject bubble = Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
+      //    Rotates Randomly
+      bubble.transform.rotation = Random.rotation;
+      //    Set Random Launch Speed
+      float launchSpeed = Random.Range(0.5f, 1.5f);
+      //  Creates Vector3 to Represent Forward Force
+      Vector3 force = spawnTransform.forward * launchSpeed;
+      bubble.GetComponent<Rigidbody>().AddForce(force);
+      bubble.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+   }
 }
+ 
